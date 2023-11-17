9008ff5d009ceb66e1effd96998336a0bce9cc76
Revaz Kapianidze <revaz.kapianidze06@geolab.edu.ge>
2023-11-17 20:02:52 +0400

    start

--- my-app_2/app/page.tsx
+++ my-app_2/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Hello React and next.je&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
